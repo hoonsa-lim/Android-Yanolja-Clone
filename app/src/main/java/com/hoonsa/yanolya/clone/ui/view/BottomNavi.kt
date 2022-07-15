f316d2bc36037792f87c8df63f4481f5a7834c69
hslim <myohoon95@gmail.com>
2022-07-15 18:47:58 +0900

    페이지 이동, jetpack navigation 추가

--- app/src/main/java/com/hoonsa/yanolya/clone/ui/view/BottomNavi.kt
+++ app/src/main/java/com/hoonsa/yanolya/clone/ui/view/BottomNavi.kt
@@ -9,12 +9,14 @@ enum class BottomNaviItemUiState(
     val icon: Int,
     @StringRes
     val label: Int,
+    val routeName: String,
 
     var isNew: Boolean = false,
+    var onClick: ((item: BottomNaviItemUiState) -> Unit) = {}
 ) {
-    Home(R.drawable.ic_tap_bottom_home, R.string.bottom_navi_home),
-    Area(R.drawable.ic_tap_bottom_area, R.string.bottom_navi_area),
-    Around(R.drawable.ic_tap_bottom_around, R.string.bottom_navi_around),
-    WishList(R.drawable.ic_tap_bottom_wishlist, R.string.bottom_navi_wish_list),
-    My(R.drawable.ic_tap_bottom_my, R.string.bottom_navi_my),
+    Home(R.drawable.ic_tap_bottom_home, R.string.bottom_navi_home, "home"),
+    Area(R.drawable.ic_tap_bottom_area, R.string.bottom_navi_area, "area"),
+    Around(R.drawable.ic_tap_bottom_around, R.string.bottom_navi_around, "around"),
+    WishList(R.drawable.ic_tap_bottom_wishlist, R.string.bottom_navi_wish_list, "wish_list"),
+    My(R.drawable.ic_tap_bottom_my, R.string.bottom_navi_my, "my"),
 }
--- app/src/main/java/com/hoonsa/yanolya/clone/ui/view/MainActivity.kt
+++ app/src/main/java/com/hoonsa/yanolya/clone/ui/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.hoonsa.yanolya.clone.ui.view
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -20,12 +22,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.hoonsa.yanolya.clone.R
 import com.hoonsa.yanolya.clone.ui.theme.*
 import timber.log.Timber
@@ -41,25 +51,124 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainView() {
+    val navController = rememberNavController()
+
     YanolyaTheme {
         Scaffold(
-            bottomBar = { YanolyaBottomNavigation() }
+            bottomBar = { YanolyaBottomNavigation(navController) }
         ) {
-
+            NavHost(navController = navController, startDestination = "home") {
+                composable("home") { HomeScreen(navController) }
+                composable("area") { AreaScreen(navController) }
+                composable("around") { AroundScreen(navController) }
+                composable("wish_list") { WishListScreen(navController) }
+                composable("my") { MyScreen(navController) }
+            }
         }
     }
 }
 
 @Composable
-fun YanolyaBottomNavigation(){
+fun HomeScreen(navController: NavController) {
+    Column() {
+        Button(onClick = {
+            Timber.d("click")
+        }) {
+            Text(text = "HomeScreen")
+        }
+    }
+}
+
+@Composable
+fun AreaScreen(navController: NavController) {
+    Column() {
+        Button(onClick = {
+            Timber.d("click")
+        }) {
+            Text(text = "AreaScreen")
+        }
+    }
+}
+
+@Composable
+fun AroundScreen(navController: NavController) {
+    Column() {
+        Button(onClick = {
+            Timber.d("click")
+        }) {
+            Text(text = "AroundScreen")
+        }
+    }
+}
+
+@Composable
+fun WishListScreen(navController: NavController) {
+    Column() {
+        Button(onClick = {
+            Timber.d("click")
+        }) {
+            Text(text = "WishListScreen")
+        }
+    }
+}
+
+@Composable
+fun MyScreen(navController: NavController) {
+    Column() {
+        Button(onClick = {
+            Timber.d("click")
+        }) {
+            Text(text = "MyScreen")
+        }
+    }
+}
+
+@Composable
+fun YanolyaBottomNavigation(navController: NavController){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
     val items = listOf(
-        BottomNaviItemUiState.Home,
-        BottomNaviItemUiState.Area,
-        BottomNaviItemUiState.Around,
-        BottomNaviItemUiState.WishList.apply { isNew = true },
-        BottomNaviItemUiState.My,
+        BottomNaviItemUiState.Home.apply {
+            onClick = {
+                testLogDestinations(currentDestination)
+                navController.navigate(this.routeName){
+
+                }
+            }
+        },
+        BottomNaviItemUiState.Area.apply {
+            onClick = {
+                testLogDestinations(currentDestination)
+                navController.navigate(this.routeName){
+                    popUpTo("home")
+                }
+            }
+        },
+        BottomNaviItemUiState.Around.apply {
+            isNew = true
+            onClick = {
+                testLogDestinations(currentDestination)
+                navController.navigate(this.routeName){
+                    popUpTo("home") { inclusive = true }
+                }
+            }
+        },
+        BottomNaviItemUiState.WishList.apply {
+            onClick = {
+                testLogDestinations(currentDestination)
+                navController.navigate(this.routeName)
+            }
+        },
+        BottomNaviItemUiState.My.apply {
+            onClick = {
+                testLogDestinations(currentDestination)
+                navController.navigate(this.routeName){
+                    launchSingleTop = true
+                }
+            }
+        },
     )
-    var selectedItem by remember { mutableStateOf(0) }
 
     return Row(
         Modifier
@@ -70,24 +179,40 @@ fun YanolyaBottomNavigation(){
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        items.forEachIndexed { index, item ->
-            BottomNaviItem(item, index, selectedItem)
-        }
+        items.forEachIndexed { index, item -> BottomNaviItem(item, currentDestination) }
+    }
+}
+
+fun testLogDestinations(currentDestination: NavDestination?) {
+    Timber.d("destination displayName == ${currentDestination?.displayName}")
+    Timber.d("destination route == ${currentDestination?.route}")
+    Timber.d("destination label == ${currentDestination?.label}")
+    Timber.d("destination id == ${currentDestination?.id}")
+    Timber.d("destination navigatorName == ${currentDestination?.navigatorName}")
+    Timber.d("destination hierarchy count == ${currentDestination?.hierarchy?.count()}")
+    currentDestination?.hierarchy?.forEach {
+        Timber.d("destination hierarchy forEach route == ${it.route}")
     }
 }
 
 @Composable
-fun BottomNaviItem(item: BottomNaviItemUiState, index: Int, selectedItem: Int) {
+fun BottomNaviItem(item: BottomNaviItemUiState, navDestination: NavDestination?) {
     val label = stringResource(id = item.label)
-    val isSelect = index == selectedItem
+    val isSelect = navDestination?.hierarchy?.any { it.route == item.routeName } == true
     val labelColor = if (isSelect) Black800 else Gray700
 
     return Column(
+        Modifier
+            .clickable(indication = null, interactionSource = MutableInteractionSource()){
+                item.onClick.invoke(item)
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Box {
             Icon(
-                modifier = Modifier.align(Alignment.Center).size(height = 30.dp, width = 39.dp),
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(height = 30.dp, width = 39.dp),
                 painter = painterResource(item.icon),
                 contentDescription = label,
                 tint = if (isSelect) MaterialTheme.colors.primary else labelColor
